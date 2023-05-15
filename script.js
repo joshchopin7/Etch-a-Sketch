57e0dadffdecbc9d4d02bef73b64755e21af28c9
joshchopin7 <126109111+joshchopin7@users.noreply.github.com>
2023-05-15 16:00:22 -0700

    finish the sketch grid

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 
 
 const container = document.querySelector('#container'); //This selects 'container' div
-
 let squares = {}; // let 'squares' be an empty object
+
 
 
 // Create a function that makes divs, styles them into squares, and appends them to 'container'
 function makeSquares(n) {
     for (i=0; i<n*n; i++) {
         let squares = document.createElement('div');
-        squares.style.cssText = 'background-color: blue; width: 20px; height: 20px;';
+        // design squares, give them an id, and add an Event Listener
+        squares.style.cssText = 'background-color: white; width: 20px; height: 20px;';
+        squares.setAttribute('id', 'squares');
+        squares.addEventListener('mouseover', mouseOver)
         container.appendChild(squares);
+        // here we define the function of mouseOver()
+        function mouseOver() {
+            squares.style.cssText = "background-color: black;  width: 20px; height: 20px;";
+          }
         // below we style the container so that it changes size to perfectly fit squares:
-        container.style.cssText += `width: ${n *22}px;`
-        container.style.cssText += `height: ${n *22}px;`
+        container.style.cssText += `width: ${n *20}px;`
+        container.style.cssText += `height: ${n *20}px;`
     }
 }
 
+
 // make a row of 16 squares
-makeSquares(16);
+makeSquares(20);
+
+
 
 
