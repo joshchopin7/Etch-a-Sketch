c7b1f05be794de34b601db972fd15621db8b8578
joshchopin7 <126109111+joshchopin7@users.noreply.github.com>
2023-05-16 15:39:45 -0700

    make sketch pad size permanent + add buttons

--- script.js
+++ script.js
@@ -10,23 +10,23 @@ function makeSquares(n) {
     for (i=0; i<n*n; i++) {
         let squares = document.createElement('div');
         // design squares, give them an id, and add an Event Listener
-        squares.style.cssText = 'background-color: white; width: 20px; height: 20px;';
+        squares.style.cssText = `background-color: white; width: ${500/n}px; height: ${500/n}px;`;
         squares.setAttribute('id', 'squares');
         squares.addEventListener('mouseover', mouseOver)
         container.appendChild(squares);
         // here we define the function of mouseOver()
         function mouseOver() {
-            squares.style.cssText = "background-color: black;  width: 20px; height: 20px;";
+            squares.style.cssText = `background-color: black;  width: ${500/n}px; height: ${500/n}px;`;
           }
         // below we style the container so that it changes size to perfectly fit squares:
-        container.style.cssText += `width: ${n *20}px;`
-        container.style.cssText += `height: ${n *20}px;`
+        container.style.cssText += `width: 500px;`
+        container.style.cssText += `height: 500px;`
     }
 }
 
 
 // make a row of 16 squares
-makeSquares(20);
+makeSquares(50);
 
 
 
