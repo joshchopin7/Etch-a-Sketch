92024666d4a75a4266b1e1344b027f9ec540dc3b
joshchopin7 <126109111+joshchopin7@users.noreply.github.com>
2023-05-16 17:12:11 -0700

    add functions to four buttons

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 const container = document.querySelector('#container'); //This selects 'container' div
-let squares = {}; // let 'squares' be an empty object
+ // let 'squares' be an empty object
 
 
 
@@ -24,10 +24,85 @@ function makeSquares(n) {
     }
 }
 
-
-// make a row of 16 squares
-makeSquares(50);
+makeSquares(16);
 
 
+const squares = document.querySelector('#squares')
 
 
+// create variable names for three buttons
+const smallBtn = document.querySelector('#small');
+const mediumBtn = document.querySelector('#medium');
+const bigBtn = document.querySelector('#big');
+
+// create a function for each size button
+function clickSmall() {
+    while (container.firstChild){
+    container.removeChild(container.lastChild);
+    };
+    makeSquares(48);
+    //clear function////////////
+    function clearAll() {
+        while (container.firstChild){
+        container.removeChild(container.lastChild);
+        };
+        makeSquares(48)
+    }
+    
+    clearBtn.addEventListener('click', clearAll);
+}
+
+function clickMed() {
+    while (container.firstChild){
+    container.removeChild(container.lastChild);
+    };
+    makeSquares(32);
+    //clear function//////////
+    function clearAll() {
+        while (container.firstChild){
+        container.removeChild(container.lastChild);
+        };
+        makeSquares(32);
+    }
+    
+    clearBtn.addEventListener('click', clearAll);
+}
+
+function clickBig() {
+    while (container.firstChild){
+    container.removeChild(container.lastChild);
+    };
+    makeSquares(16);
+    // clear function //////////
+    function clearAll() {
+        while (container.firstChild){
+        container.removeChild(container.lastChild);
+        };
+        makeSquares(16);
+    }
+    
+    clearBtn.addEventListener('click', clearAll);
+}
+
+// add event listen for each button
+
+smallBtn.addEventListener('click', clickSmall);
+mediumBtn.addEventListener('click', clickMed);
+bigBtn.addEventListener('click', clickBig);
+
+
+// create variable for clear
+const clearBtn = document.querySelector('#clear');
+
+// This function works on load of the first makeSquares()
+
+function clearAll() {
+    while (container.firstChild){
+    container.removeChild(container.lastChild);
+    };
+    makeSquares(16);
+}
+
+clearBtn.addEventListener('click', clearAll);
+
+// I also placed a clear function inside each size button function so that the sizing remains the same /////////////////////////////////////
